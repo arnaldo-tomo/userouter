d7f9e833fb3081b8c9bee03d9eb326e7123bcd4d
Arnaldo tomo <arnaldotomo@gmail.com>
2024-02-01 08:37:42 +0200

    cn

---
+++ app/dashboard/[id]/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Test({params}:any) {
+
+    console.log(params)
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'Use client';
+import { useRouter } from "next/navigation"
+import { User } from "../interface/interface";
+
+export default function dashboard (){
+    return(
+        <div>
+            <h1>Hello World Dashboard</h1>
+        </div>
+    )
+}
